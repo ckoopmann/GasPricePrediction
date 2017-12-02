d2ce2f0c0a92a274565815ba7e984fa395778366
ckoopmann <c.k.e.koopmann@gmail.com>
2017-12-02 18:56:28 +0100

    aws instructions install scikit learn

--- Python/LevelPrediction/aws_commands.sh
+++ Python/LevelPrediction/aws_commands.sh
@@ -1,21 +1,21 @@
 source activate tensorflow_p36
 conda install scikit-learn
-python3 level_par_tuning_nocv.py
+python level_par_tuning_nocv.py
 git pull
 git add --all
 git commit -m "New AWS results - Level Par Tuning"
 git push
-python3 level_var_selection_nocv.py
+python level_var_selection_nocv.py
 git pull
 git add --all
 git commit -m "New AWS results - Level Var Selection"
 git push
-python3 level_multivar_par_tuning_nocv.py
+python level_multivar_par_tuning_nocv.py
 git pull
 git add --all
 git commit -m "New AWS results - Level Multivar Par Tuning"
 git push
-python3 level_eval_cv.py
+python level_eval_cv.py
 git pull
 git add --all
 git commit -m "New AWS results - Level Evaluation"
